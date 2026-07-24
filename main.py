8642f141fb32c214dc7fa2d76f6ff8cf594a7bb9
asmaabe01 <asmaaebequi@gmail.com>
2026-07-24 15:08:20 +0800

    Add task creation and listing endpoints

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Task(BaseModel):
+    title: str
+    completed: bool = False
+
+
+tasks = []
 
 
 @app.get("/")
 def home():
     return {"message": "Welcome to my Task Tracker API"}
+
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+
+@app.post("/tasks", status_code=status.HTTP_201_CREATED)
+def create_task(task: Task):
+    new_task = {
+        "id": len(tasks) + 1,
+        "title": task.title,
+        "completed": task.completed
+    }
+
+    tasks.append(new_task)
+    return new_task
